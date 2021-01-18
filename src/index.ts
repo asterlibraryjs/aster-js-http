82fe04f0e6ac5c44bb3099876ded55282b4b7e6f
Sébastien Doucet <sebdoucet@gmail.com>
2021-01-17 21:36:47 -0500

    Fix package

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export * from "./handlers/irequest-handler";
+export * from "./handlers/delegating-request-handler";
+export * from "./handlers/cache-request-handler";
+export * from "./handlers/configure-request-handler";
+export * from "./handlers/timeout-request-handler";
+export * from "./http-body";
+export * from "./http-client";
